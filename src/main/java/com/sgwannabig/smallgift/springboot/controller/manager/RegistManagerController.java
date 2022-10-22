c81195842cbbaac191e82e838473ed7921be5b3a
hojoon <ujuj0202@gmail.com>
2022-10-22 15:59:53 +0900

    fix: 매니저 등록 기능

--- src/main/java/com/sgwannabig/smallgift/springboot/controller/manager/RegistManagerController.java
+++ src/main/java/com/sgwannabig/smallgift/springboot/controller/manager/RegistManagerController.java
@@ -1,19 +1,29 @@
 package com.sgwannabig.smallgift.springboot.controller.manager;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.sgwannabig.smallgift.springboot.config.jwt.JwtProperties;
 import com.sgwannabig.smallgift.springboot.domain.Manager;
+import com.sgwannabig.smallgift.springboot.domain.Shop;
 import com.sgwannabig.smallgift.springboot.dto.manager.request.RegistManagerDto;
 import com.sgwannabig.smallgift.springboot.dto.manager.response.RegistManagerResponseDto;
+import com.sgwannabig.smallgift.springboot.service.ResponseService;
 import com.sgwannabig.smallgift.springboot.service.manager.RegistManagerCommand;
 import com.sgwannabig.smallgift.springboot.service.manager.RegistManagerUsecase;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
+import com.sgwannabig.smallgift.springboot.service.result.SingleResult;
+import com.sgwannabig.smallgift.springboot.service.shop.RegistShopCommand;
+import com.sgwannabig.smallgift.springboot.service.shop.RegistShopUsecase;
+
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,31 +35,40 @@ import org.springframework.web.multipart.MultipartFile;
 public class RegistManagerController {
 
   private final RegistManagerUsecase registManagerUsecase;
+  private final RegistShopUsecase registShopUsecase;
+  private final ResponseService responseService;
 
   @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   @ApiOperation(value = "매니저 등록", notes = "매니저 등록을 위한 정보를 보내준")
-  @ApiImplicitParams({
-      @ApiImplicitParam(name = "registManager", value = "매니저 등록 정보", required = true, paramType = "body", dataType = "RegistManagerDto"),
-      @ApiImplicitParam(name = "businessRegistration", value = "매니저 등록을 사업자등록증 이미지 파일", required = true,dataType = "__file", paramType = "form"),
-      @ApiImplicitParam(name = "mailOrderSalesRegistration", value = "매니저 등록을 통신판매 신고증 이미지 파일", required = true, dataType = "__file", paramType = "form")
-  })
   @ApiResponses({
       @ApiResponse(code = 200, message = "성공"),
       @ApiResponse(code = 500, message = "서버에러"),
       @ApiResponse(code = 409, message = "이미 존재하는 사업자입니다"),
       @ApiResponse(code = 400, message = "잘못된 요청입니다")
   })
-  public ResponseEntity<RegistManagerResponseDto> registManager(
+  public ResponseEntity<SingleResult> registManager(
+      @RequestHeader(value = JwtProperties.HEADER_STRING, required = false)
+      String authorization,
       @RequestPart("registManager")
       RegistManagerDto registManagerDto,
-      @RequestPart
+      @RequestPart(name = "businessRegistration")
       MultipartFile businessRegistration,
-      @RequestPart
-      MultipartFile mailOrderSalesRegistration){
+      @RequestPart(name = "mailOrderSalesRegistration")
+      MultipartFile mailOrderSalesRegistration) {
 
+    String jwt = authorization.replace(JwtProperties.TOKEN_PREFIX, "");
+
+    //Security Context 사용 방법으로 변경 고려
+    Long userId = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET)).build().verify(jwt)
+        .getClaim("id").asLong();
+    Manager registManager = registManagerDto.toEntity();
+    registManager.setUserId(userId);
     Manager manager = registManagerUsecase.apply(
-        new RegistManagerCommand(registManagerDto.toEntity(), businessRegistration,
+        new RegistManagerCommand(registManager, businessRegistration,
             mailOrderSalesRegistration));
-    return ResponseEntity.ok(new RegistManagerResponseDto(manager.getId()));
+    Shop shop = registShopUsecase.apply(new RegistShopCommand(manager));
+
+    return ResponseEntity.ok(responseService.getSingleResult(
+        new RegistManagerResponseDto(manager.getId(), shop.getId())));
   }
 }
--- src/main/java/com/sgwannabig/smallgift/springboot/dto/manager/request/RegistManagerDto.java
+++ src/main/java/com/sgwannabig/smallgift/springboot/dto/manager/request/RegistManagerDto.java
@@ -10,36 +10,37 @@ import lombok.NoArgsConstructor;
 
 @Getter
 public class RegistManagerDto {
+
   @ApiModelProperty(example = "홍길동")
   @ApiParam(value = "대표명", required = true)
   @NotNull
   private String username;
 
-  @ApiModelProperty(example="엽떡")
+  @ApiModelProperty(example = "엽떡")
   @ApiParam(value = "상호명", required = true)
   @NotNull
   private String businessName;
-  @ApiModelProperty(example="서울시 마포구 양화로3길 66")
+  @ApiModelProperty(example = "서울시 마포구 양화로3길 66")
   @ApiParam(value = "주소", required = true)
   @NotNull
   private String address;
-  @ApiModelProperty(example="999-99-999000")
+  @ApiModelProperty(example = "999-99-999000")
   @ApiParam(value = "사업자 등록번호", required = true)
   @NotNull
   private String businessTel;
-  @ApiModelProperty(example="종목 업태")
+  @ApiModelProperty(example = "종목 업태")
   @ApiParam(value = "종목 업태", required = true)
   @NotNull
   private String businessType;
-  @ApiModelProperty(example="홍길동")
+  @ApiModelProperty(example = "홍길동")
   @ApiParam(value = "예금주명", required = true)
   @NotNull
   private String accountHolder;
-  @ApiModelProperty(example="우리은행")
+  @ApiModelProperty(example = "우리은행")
   @ApiParam(value = "은행", required = true)
   @NotNull
   private String settlementBank;
-  @ApiModelProperty(example="1002-000-000000")
+  @ApiModelProperty(example = "1002-000-000000")
   @ApiParam(value = "계좌", required = true)
   @NotNull
   private String settlementAccount;
