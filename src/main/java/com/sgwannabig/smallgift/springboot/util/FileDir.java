5d352334d3725ba1bf307a3516589590098fbb84
hojoon <ujuj0202@gmail.com>
2022-10-22 16:01:13 +0900

    fix: 못사용하는 기능 주석 처리

--- src/main/java/com/sgwannabig/smallgift/springboot/util/FileDir.java
+++ src/main/java/com/sgwannabig/smallgift/springboot/util/FileDir.java
@@ -1,14 +1,14 @@
-package com.sgwannabig.smallgift.springboot.util;
-
-import lombok.Getter;
-
-@Getter
-public enum FileDir {
-  REGIST_MANAGER("manager/regist");
-
-  FileDir(String dir) {
-    this.dir = dir;
-  }
-
-  private String dir;
-}
+//package com.sgwannabig.smallgift.springboot.util;
+//
+//import lombok.Getter;
+//
+//@Getter
+//public enum FileDir {
+//  REGIST_MANAGER("manager/regist");
+//
+//  FileDir(String dir) {
+//    this.dir = dir;
+//  }
+//
+//  private String dir;
+//}
