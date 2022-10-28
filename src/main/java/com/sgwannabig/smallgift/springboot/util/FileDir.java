20a1ed306b4d2028925de42b4bd6fb542ecc2747
hojoon <ujuj0202@gmail.com>
2022-10-28 19:12:51 +0900

    fix: s3 업로드 구현

--- src/main/java/com/sgwannabig/smallgift/springboot/util/FileDir.java
+++ src/main/java/com/sgwannabig/smallgift/springboot/util/FileDir.java
@@ -1,14 +1,15 @@
-//package com.sgwannabig.smallgift.springboot.util;
-//
-//import lombok.Getter;
-//
-//@Getter
-//public enum FileDir {
-//  REGIST_MANAGER("manager/regist");
-//
-//  FileDir(String dir) {
-//    this.dir = dir;
-//  }
-//
-//  private String dir;
-//}
+package com.sgwannabig.smallgift.springboot.util;
+
+import lombok.Getter;
+
+@Getter
+public enum FileDir {
+  REGIST_MANAGER("image/manager"),
+  REGIST_PRODUCT("image/product");
+
+  FileDir(String dir) {
+    this.dir = dir;
+  }
+
+  private String dir;
+}
