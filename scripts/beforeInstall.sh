600ddb272347526a1a913f99ab9e976e7fac0612
hojoon <ujuj0202@gmail.com>
2022-11-05 02:00:38 +0900

    fix: deploy 수정

---
+++ scripts/beforeInstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# I want to make sure that the directory is clean and has nothing left over from
+# previous deployments. The servers auto scale so the directory may or may not
+# exist.
+if [ -d /home/ec2-user/app/zip/ ]; then
+    rm -rf /home/ec2-user/app/zip/
+fi
+mkdir -vp /home/ec2-user/app/zip/
